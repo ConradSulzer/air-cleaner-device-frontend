4643dd97bf964ac6ca16edf1f5796cbc70a67bc0
Conrad Sulzer <conrad.sulzer@gmail.com>
2021-01-21 20:50:13 -0600

    added htaccess to root

--- app.js
+++ app.js
@@ -5,17 +5,14 @@ const app = {
         console.log('app initialized')
 
         this.getData();
-        console.log(sections)
     },
 
     getData: async function () {
         console.log('getting data...')
-        const proxy = 'https://cors-anywhere.herokuapp.com/'
         const url = "http://127.0.0.1/aircontrol/get.php";
 
         const response = await fetch(url, {
             method: 'GET',
-            origin: '*'
         })
 
         console.log('Response', response);
